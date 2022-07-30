1581d8cd29e3104502fe057abbb846c8d4a88ac7
lmk123568 <lmk123568@qq.com>
2022-07-31 00:42:25 +0800

    upgrade tests

---
+++ tests/test_models.py
@@ -0,0 +1,21 @@
+import pytest
+import paddle
+
+class TestModel():
+
+    def setup_class(self):
+        """
+        环境初始化
+        """
+        paddle.set_device('cpu')
+        self.dummy_img = paddle.randn([4,3,224,224])
+        self.dummy_tensor = paddle.to_tensor(self.dummy_img)
+        self.model = paddle.vision.models.resnet50(pretrained=True)
+
+    #@pytest.mark.skip(reason='skip for debug')
+    def test_out_shape(self):
+        out = self.model(self.dummy_tensor)
+        assert out.shape == [4, 1000]
+
+    def teardown_class(self):
+        pass
